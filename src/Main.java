277df836f1c2b8dd680c936246f5e270ac8b49d0
aaf <alualb1634@ieselcaminas.org>
2022-12-19 15:31:28 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         SortedTextFile s = new SortedTextFile("test.txt");
-        s.put("ccccc");
-        s.put("aaaaa");
-        s.put("bbbbb");
-        s.put("ddddd");
-        s.print();
+        SortedTextFile s1 = new SortedTextFile("test2.txt");
+        s.merge("test.txt", "test2.txt", "out.txt");
 
     }
 }
--- src/SortedTextFile.java
+++ src/SortedTextFile.java
@@ -9,10 +9,11 @@ public class SortedTextFile {
     public SortedTextFile(String fileName) throws IOException {
         this.fileName = fileName;
         File file = new File(fileName);
-        if (!file.exists()){
+        if (!file.exists()) {
             file.createNewFile();
         }
     }
+
     public String getName() {
         return fileName;
     }
@@ -34,31 +35,32 @@ public class SortedTextFile {
                 }
                 output.println(line);
             }
-            if (!alreadyInserted){
+            if (!alreadyInserted) {
                 output.println(newLine);
             }
             file.delete();
             temp.renameTo(file);
-        }finally{
-                if (input != null) {
-                    input.close();
-                }
-                if (output != null) {
-                    output.close();
-                }
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
+            }
         }
     }
+
     public int getNumElements() throws IOException {
         BufferedReader in = null;
         String line;
-        try{
+        try {
             in = new BufferedReader(new FileReader(fileName));
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 numElements++;
             }
             return numElements;
         } finally {
-            if ((in != null)){
+            if ((in != null)) {
                 in.close();
             }
         }
@@ -66,61 +68,64 @@ public class SortedTextFile {
 
     public String getElementAt(int elementAt) throws IOException {
         BufferedReader in = null;
-        try{
+        try {
             String line;
             int counter = 0;
             in = new BufferedReader(new FileReader(fileName));
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 counter++;
-                if (counter == elementAt){
+                if (counter == elementAt) {
                     return line;
                 }
             }
             return null;
         } finally {
-            if ((in != null)){
+            if ((in != null)) {
                 in.close();
             }
         }
     }
+
     public void removeElementAt(int numLine) throws IOException {
         File file = new File(fileName);
         File temp = new File("temp.txt");
         BufferedReader in = null;
         PrintWriter out = null;
-        try{
+        try {
             String line;
             int counter = 0;
             in = new BufferedReader(new FileReader(file));
             out = new PrintWriter(new FileWriter(temp));
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 counter++;
-                if (counter != numLine){
+                if (counter != numLine) {
                     out.println(line);
                 }
             }
             file.delete();
             temp.renameTo(file);
         } finally {
-            if (in != null){
+            if (in != null) {
                 in.close();
             }
-            if (out != null){
+            if (out != null) {
                 out.close();
             }
         }
     }
+
     public boolean isEmpty() throws IOException {
         BufferedReader in = null;
         try {
             in = new BufferedReader(new FileReader(fileName));
             return in.readLine() == null;
         } finally {
-            if (in != null){
+            if (in != null) {
                 in.close();
             }
         }
     }
+
     public void emptyFile() throws IOException {
         File file = new File(fileName);
         file.delete();
@@ -129,20 +134,84 @@ public class SortedTextFile {
 
     public void print() throws IOException {
         BufferedReader in = null;
-        try{
+        try {
             String line;
             in = new BufferedReader(new FileReader(fileName));
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 System.out.println(line);
             }
         } finally {
-            if ((in != null)){
+            if ((in != null)) {
                 in.close();
             }
         }
     }
 
-    public boolean existElement (String line){
+    public boolean existElement(String searchedLine) throws IOException {
+        BufferedReader in = null;
+        try {
+            String line;
+            in = new BufferedReader(new FileReader(fileName));
+            while ((line = in.readLine()) != null) {
+                if (line.compareTo(searchedLine) > 0) {
+                    return false;
+                } else if (line.equals(searchedLine)) {
+                    return true;
+                }
+            }
+            return false;
+        } finally {
+            if ((in != null)) {
+                in.close();
+            }
+        }
+    }
 
+    public static void print(String fileName) throws IOException {
+        SortedTextFile s = new SortedTextFile(fileName);
+        s.print();
+    }
+
+    public static void merge(String fileName1, String fileName2, String fileNameOut) throws IOException {
+        BufferedReader input1 = null;
+        BufferedReader input2 = null;
+        PrintWriter output = null;
+        try {
+            input1 = new BufferedReader(new FileReader(fileName1));
+            input2 = new BufferedReader(new FileReader(fileName2));
+            output = new PrintWriter(new FileWriter(fileNameOut));
+            String line1 = input1.readLine();
+            String line2 = input2.readLine();
+            while (line1 != null || line2 != null) {
+                if (line1 == null) {
+                    output.println(line2);
+                    line2 = input2.readLine();
+
+                } else if (line2 == null) {
+                    output.println(line1);
+                    line1 = input1.readLine();
+                } else if (line1.compareTo(line2) <= 0) {
+                    output.println(line1);
+                    line1 = input1.readLine();
+                } else {
+                    output.println(line2);
+                    line2 = input2.readLine();
+                }
+            }
+        } finally {
+            if (input1 != null) {
+                input1.close();
+            }
+            if (input2 != null) {
+                input2.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+
+    public static void merge(SortedTextFile sf1, SortedTextFile sf2, String fileNameOut) throws IOException {
+        merge(sf1.getName(), sf2.getName(), fileNameOut);
     }
 }
